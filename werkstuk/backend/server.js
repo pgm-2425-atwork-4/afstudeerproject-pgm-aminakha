3f1f8144955b65ebb1de8be93a5e6a3249095fff
pgm-aminakha <amin.akhayad@student.arteveldehs.be>
2025-03-13 23:42:51 +0100

    update CORS middleware to allow requests from any origin

--- werkstuk/backend/server.js
+++ werkstuk/backend/server.js
@@ -10,7 +10,7 @@ const bcrypt = require("bcrypt");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({origin:"*"}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.get('/', (req, res) => {
