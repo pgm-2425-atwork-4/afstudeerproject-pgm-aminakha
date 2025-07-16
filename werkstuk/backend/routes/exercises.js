0592dfaf8d14dd55504cea3334fc26b610fca612
pgm-aminakha <amin.akhayad@student.arteveldehs.be>
2025-07-16 02:14:09 +0200

    fix: import uploadImage middleware in exercises route

--- werkstuk/backend/routes/exercises.js
+++ werkstuk/backend/routes/exercises.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { db } = require('../config/db');  
+const { uploadImage } = require("../middlewares/multerConfig");
 
 router.get("/categories", (req, res) => {
     db.query("SELECT * FROM exercise_categories", (err, results) => {
