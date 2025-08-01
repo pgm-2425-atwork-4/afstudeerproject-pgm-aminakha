bd0239d6a2d2bbef851e79f266d5088f24308f5e
pgm-aminakha <amin.akhayad@student.arteveldehs.be>
2025-08-01 21:21:23 +0200

    fix: Update multer configuration and enhance alert styling with icons

--- werkstuk/backend/routes/exercises.js
+++ werkstuk/backend/routes/exercises.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { db } = require('../config/db');  
-const { uploadImage, exerciseImages } = require("../middlewares/multerConfig");
+const { exerciseImages, upload } = require("../middlewares/multerConfig");
 
 router.get("/categories", (req, res) => {
     db.query("SELECT * FROM exercise_categories", (err, results) => {
